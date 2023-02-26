b172edd92e39f8a2857877821f0c98818038cb47
ajju <ajju.me@gmail.com>
2023-02-26 18:13:15 +0000

    shopping list or Todo list changes completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ListItem } from './list-item.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angular-todo-list';
+  title = 'My Shopping List';
+  listItems : ListItem[] = [];
+  newItem : string;
+
+
+  addItem(){
+    if(this.newItem){
+      let newItemForTodo = new ListItem();
+      newItemForTodo.itemName = this.newItem;
+      newItemForTodo.isCompleted = false;
+      this.listItems.push(newItemForTodo)
+      this.newItem = "";
+    }
+    else{
+      alert('Please enter an item name');
+    }
+  }
+
+  itemCompleted(lineNumber:number){
+    this.listItems[lineNumber].isCompleted = !this.listItems[lineNumber].isCompleted;
+  }
+
+  removeItem(lineNumber:number){
+    this.listItems = this.listItems.filter((v, i) => i!= lineNumber);
+  }
+  
 }
---
+++ src/app/list-item.model.ts
@@ -0,0 +1,4 @@
+export class ListItem {
+    itemName:string;
+    isCompleted:boolean;
+}
